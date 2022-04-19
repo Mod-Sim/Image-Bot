9234bbeadf53bd600216fc3f293c086bf3603d5b
Tyler-Seliber <tylerseliber@gmail.com>
2022-04-18 20:41:48 -0400

    Tesing deploy-command automatically

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
+		.setName('pingme')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Right back at ya!');
