7ee91bdce68cb3ce767db0e3fc31a5151ef39190
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2022-04-18 23:22:00 -0400

    Deploy command automatically

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
+		.setName('pingify')
 		.setDescription('What could this do?'),
 	async execute(interaction) {
 		await interaction.reply('Poke! 🤗');
