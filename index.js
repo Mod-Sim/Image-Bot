d313a7dd5a4f97e77e1b65d72ad279e85a2b7c2e
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2022-04-19 09:47:16 -0400

    Removed unnecessary comments

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const config = require('./env-var');
 const token = config.getConfig().token;
 const deployCommands = require('./deploy-commands');
 
-// console.log('Deploying commands...');
-// deployCommands();
-// console.log('Commands deployed.');
-
-
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
