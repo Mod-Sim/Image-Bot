9ff9b87333f2e3302aef0d36663c662f45fb701a
Tyler-Seliber <tylerseliber@gmail.com>
2022-04-18 20:38:45 -0400

    Testing live deploy

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply('Right back at ya!');
 	},
 };
