6a82faed3a5b6adf081fc19d6f559e52883b2fe1
Tyler-Seliber <tylerseliber@gmail.com>
2022-04-20 22:53:08 -0400

    Can send an image from the internet

--- commands/search.js
+++ commands/search.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment, MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,9 +7,15 @@ module.exports = {
 		.setDescription('Search for an image')
 		.addStringOption(option =>
 			option.setName('input')
-		.setDescription('The search term')
-		.setRequired(true)),
+				.setDescription('The search term')
+				.setRequired(true)),
+		
 	async execute(interaction) {
-		await interaction.reply('Here\'s a picture of ' + interaction.options.getString('input') + '! 🖼');
+		const exampleEmbed = new MessageEmbed()
+			.setTitle(interaction.options.getString('input'))
+			.setImage('https://www.stevens.edu/sites/stevens_edu/files/styles/home_page_carousel_item_980x551/public/Drone%20Video%20Frame%207-crop.jpg?itok=HvxaqNMP');
+
+		await interaction.reply({ embeds: [exampleEmbed] });
+
 	},
 };
