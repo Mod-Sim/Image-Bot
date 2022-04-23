7de6fb5f13cf4d7a8817dfd0aa6d0d0084bc5678
Tyler-Seliber <tylerseliber@gmail.com>
2022-04-23 14:49:53 -0400

    Specify deploy commands to guild or globally

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,6 +6,9 @@ const config = require('./env-var');
 const clientId = config.getConfig().clientId;
 const guildId = config.getConfig().guildId;
 const token = config.getConfig().token;
+// Get arguments from command input to determine environment state
+const args = process.argv.slice(2);
+const env_state = args[0];
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -19,21 +22,27 @@ const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
 	try {
-		console.log('Started refreshing application (/) commands.');
+		console.log(`Started refreshing ${env_state} application (/) commands.`);
 
-		// Deploy commands to the development guild immediately
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-		console.log('Successfully reloaded application (/) commands to guild.');
+		if (env_state === 'dev') {
+			// Deploy commands to the development guild immediately
+			await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: commands },
 
-		// Deploy commands globally to production
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-		console.log('Successfully reloaded application (/) commands globally.');
+			);
+			console.log(`Successfully reloaded application (/) commands to guild ${guildId}.`);
+
+		} else if (env_state === 'prod') {
+			// Deploy commands globally to production
+			await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: commands },
+			);
+			console.log('Successfully reloaded application (/) commands globally.');
+		} else {
+			throw new Error(`Invalid environment state: ${env_state}. Must be 'dev' or 'prod'.`);
+		}
 	} catch (error) {
 		console.error(error);
 	}
